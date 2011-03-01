6166dfe3cd74f57e51d51073b5e8216849bfd2cf
James Trowbridge <jamestrowbridge@james-trowbridges-macbook-pro.local>
2011-02-28 21:11:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickNfl::Application.config.secret_token = '2ea27913d9159c7dd067eb9112badc7001675dee0006dfa488b7cd48ba08c8323ea3ed1ee2e7e14f3f72c822a53ee7fc0fa89709b38d0e78927378e55cac0d5f'
